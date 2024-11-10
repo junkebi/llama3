2ac3d67e3abb4fe5b8413e97d8d3eba97383cf0e
junkebi <54919131+junkebi@users.noreply.github.com>
2024-11-10 12:29:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.title('LangChain 기반 영어 번역 애플리케이션')
 user_input = st.text_input("영어 문장을 넣어주세요:")
 
 if st.button('전송'):
-    remote = RemoteRunnable(url="http://localhost:8000/translate")
+    remote = RemoteRunnable(url="https://upward-lacewing-selected.ngrok-free.app/translate")
     result = remote.invoke({"input": user_input})
     st.write("응답:", result)
